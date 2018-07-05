3cce69a089fbb789f110e1f1e2fb789b1b2dd441
Mohamed Sharaf <mosharaf@microsoft.com>
2018-07-05 15:32:33 -0400

    Add project files.

---
+++ RequestInspector/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Headers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RequestInspector.Pages
+{
+    public class IndexModel : PageModel
+    {
+       
+
+        public void OnGet()
+        {
+          
+
+
+        }
+    }
+   
+
+}
